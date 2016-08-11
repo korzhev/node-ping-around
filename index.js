5e15836efbde6ca421b2b7faa5d1533af3654b9d
Коржев В.Н <vn.korzhev@tensor.ru>
2016-08-11 12:12:19 +0300

    fixes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var ping = require('net-ping');
 function PingAround(ipList, options) {
   this.ipList = ipList;
   var sockOptions = options.socketOptions || {};
+  this.isActive = false;
   this._socketOptions = {
     networkProtocol: ping.NetworkProtocol[sockOptions.networkProtocol || 'IPv6'],
     retries: sockOptions.retries || 1,
@@ -23,27 +24,41 @@ function PingAround(ipList, options) {
       + (options.sessionIdSalt || 0);
     this._socketPool.push(ping.createSession(this._socketOptions));
   }
+
+  this.start = this.start.bind(this);
+
 }
 
 util.inherits(PingAround, EE);
 
 PingAround.prototype.start = function paStart() {
-  var self = this;
-  if (self.ipList.length < self._socketPoolSize) {
-    self._startTimeout = setTimeout(function() { self.start(); }, 5000);
+  //var self = this;
+  if (this.ipList.length < this._socketPoolSize) {
+    this._startTimeout = setTimeout(this.start, 5000);
     return this;
   }
-  while (self._startIteration < self._socketPool.length) {
-    self._ping(self._startIteration++);
+  while (this._startIteration < this._socketPool.length) {
+    this._ping(this._startIteration++);
   }
+  this.isActive = true;
   this.emit('start');
   return this;
 };
 
 PingAround.prototype.stop = function paStop() {
-  this.emit('stop');
   clearTimeout(this._startTimeout);
+  this.isActive = false;
+  this.emit('stop');
 };
+
+PingAround.prototype._next = function paNext(host, index) {
+  if (!this.ipList.length) return this.stop();
+  this._disableIp(host);
+  this._currentHostIndex++;
+  this._ping(index);
+  return this;
+};
+
 PingAround.prototype._ping = function paPing(index) {
   //var mr = Math.floor(Math.random() * 1000);
   var ms = 0;
@@ -69,17 +84,13 @@ PingAround.prototype._ping = function paPing(index) {
       } else {
         ms = rcvd - sent;
         if (ms >= self._socketOptions.timeout) {
-          self._disableIp(host);
-          self._currentHostIndex++;
-          self._ping(index);
+          self._next(host, index);
           return;
         }
         //console.log('socket №', index, '-> host № ', hostIndex, host, ms + 'ms');
         self.emit('data', ms, host);
       }
-      self._disableIp(host);
-      self._currentHostIndex++;
-      self._ping(index);
+      self._next(host, index);
     }
   );
 };
@@ -106,11 +117,15 @@ PingAround.prototype.removeHost = function paRemoveHost(ip) {
       index = self.ipList.indexOf(item);
       if (~index) {
         self.ipList = self.ipList.splice(index, 1);
+        self._disableIp(ip);
       }
     });
   } else {
     index = self.ipList.indexOf(ip);
-    if (~index) this.ipList.splice(index, 1);
+    if (~index) {
+      self.ipList.splice(index, 1);
+      self._disableIp(ip);
+    }
   }
   return this;
 };
